280437ec8cfcd3f96be1db8d363fba93041468c0
Inna Kirichenko <dawavovak@gmail.com>
2022-12-06 15:17:28 +0100

    update t6

--- js/task-06.js
+++ js/task-06.js
@@ -15,9 +15,11 @@ inputEl.addEventListener ('blur', onInputBlur);
 
 function onInputBlur (event) {
   if  (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.remove ('invalid');
 inputEl.classList.add ('valid');
   } else {
-    inputEl.classList.add ('invalid')
+    inputEl.classList.remove ('valid');
+    inputEl.classList.add ('invalid');
   }
 console.log ('no');
 }
