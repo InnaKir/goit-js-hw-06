b473daac728c0a759a0657698556f95412491217
Inna Kirichenko <dawavovak@gmail.com>
2022-12-07 17:41:23 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const btnCreate =  document.querySelector('[data-create]');
+const btnDestroy =  document.querySelector('[data-destroy]');
+
+const boxes = document.querySelector ('#boxes');
+
+let 
+
+function createBoxes(amount)
